39fa2471bb4441fdddb19821ea8a06116eb10d37
XezMet <office@xezmet.at>
2025-11-29 22:18:59 +0100

    Remove temporary test files

--- admin/check_conn.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once '../database/db_connect.php'; // Assuming this exists, or I need to check how connection is made.
-// Checking fix_db.php to see how connection is made usually.
-// Actually, let's check fix_db.php first to be sure about connection details.
-?>
