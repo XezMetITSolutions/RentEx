f5d50269da56b416c4c785dff15012bde3823745
XezMet <office@xezmet.at>
2026-06-16 20:37:19 +0200

    Fix Stripe API version

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -3,6 +3,6 @@ import Stripe from 'stripe';
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY || 'sk_test_placeholder';
 
 export const stripe = new Stripe(stripeSecretKey, {
-    apiVersion: '2025-01-27-acacia' as any, // Use latest stable
+    apiVersion: '2023-10-16' as any,
     typescript: true,
 });
